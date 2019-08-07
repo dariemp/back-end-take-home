b6019442a4c1c91ecbb2f1e1b7f58b6aa2b1870a
Dariem Pérez Herrera <dariemp@gmail.com>
2019-08-07 18:59:59 -0400

    Tets graph loads all airlines

--- tests/unit/test_routes_graph.py
+++ tests/unit/test_routes_graph.py
@@ -9,3 +9,17 @@ class RoutesGraphTestCase(unittest.TestCase):
         os.environ['DATADIR'] = 'test'
         routes_graph = RoutesGraph()
         self.assertTrue(routes_graph.airlines[0] == 'AC')
+
+    def test_load_data_with_full_data_loads_six_airlines(self):
+        import os
+
+        os.environ['DATADIR'] = 'full'
+        routes_graph = RoutesGraph()
+        self.assertEqual(len(routes_graph.airlines), 6)
+
+    def test_load_data_with_full_data_loads_turkish_airlines(self):
+        import os
+
+        os.environ['DATADIR'] = 'full'
+        routes_graph = RoutesGraph()
+        self.assertEqual(routes_graph.airlines[3], 'TK')
