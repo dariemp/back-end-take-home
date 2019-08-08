e502d0e128f7a3869662f85b5cfa7f424e6dc0d7
Dariem Pérez Herrera <dariemp@gmail.com>
2019-08-07 22:59:12 -0400

    Refactor to use a more functional approach to decouple code and make class refactoring easier

--- routes_graph.py
+++ routes_graph.py
@@ -12,49 +12,51 @@ class RoutesGraph(object):
 
     @property
     def airports(self):
-        return set(self._airports_with_routes.keys())
+        return set(self._airports_connections.keys())
 
     def load_data(self):
         data_sub_directory = os.environ.get('DATADIR', 'full')
         data_path = os.path.join(os.getcwd(), 'data', data_sub_directory)
-        self._load_airlines(data_path)
-        self._load_airports(data_path)
+        self._airlines = self._load_airlines(data_path)
+        self._airports_connections = self._load_airports(data_path)
 
     def get_airport_connections(self, airport_code):
         try:
-            return self._airports_with_routes[airport_code]
+            return self._airports_connections[airport_code]
         except KeyError:
             from exceptions import AirportNotFound
             raise AirportNotFound()
 
     def _load_airlines(self, data_path):
-        self._airlines = set()
         airlines_file_path = os.path.join(data_path, 'airlines.csv')
-        self._load_airlines_from_csv(airlines_file_path)
+        return self._load_airlines_from_csv(airlines_file_path)
 
     def _load_airports(self, data_path):
-        self._airports_with_routes = {}
         airports_file_path = os.path.join(data_path, 'airports.csv')
-        self._load_airports_from_csv(airports_file_path)
+        return self._load_airports_from_csv(airports_file_path)
 
     def _load_airlines_from_csv(self, airlines_file_path):
-        self._load_csv(airlines_file_path, self._load_airline)
+        airlines_set = set()
+        self._load_csv(airlines_file_path, airlines_set, self._load_airline)
+        return airlines_set
 
     def _load_airports_from_csv(self, airports_file_path):
-        self._load_csv(airports_file_path, self._load_airport)
+        airports_connections = {}
+        self._load_csv(airports_file_path, airports_connections, self._load_airport)
+        return airports_connections
 
-    def _load_csv(self, file_path, row_proccessing_function):
+    def _load_csv(self, file_path, data_bucket, row_proccessing_function):
         with open(file_path, 'r', newline='') as f:
             csv_reader = csv.reader(f)
             self._skip_header(csv_reader)
             for row in csv_reader:
-                row_proccessing_function(row)
+                row_proccessing_function(data_bucket, row)
 
     def _skip_header(self, csv_reader):
         next(csv_reader)
 
-    def _load_airline(self, row):
-        self._airlines.add(row[1])
+    def _load_airline(self, airlines_set, row):
+        airlines_set.add(row[1])
 
-    def _load_airport(self, row):
-        self._airports_with_routes[row[3]] = []
+    def _load_airport(self, airports_dict, row):
+        airports_dict[row[3]] = []
